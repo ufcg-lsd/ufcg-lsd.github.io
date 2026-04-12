ea245af333f1c9603e3f91c67088ba30e2fa0616
rodrigoec <rodrigoecavalcanti@gmail.com>
2026-04-12 12:32:14 -0300

    Refact: Makes the images taller

--- src/components/PhotoGalleryGrid.tsx
+++ src/components/PhotoGalleryGrid.tsx
@@ -35,7 +35,7 @@ export const PhotoGalleryGrid: React.FC<{ photos: IPhotoGallery[] }> = ({
 
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 auto-rows-[240px] sm:auto-rows-[200px]">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 auto-rows-[300px] sm:auto-rows-[200px]">
         {paginated.map((photo, i) => (
           <PhotoCard
             key={photo.title + i}
